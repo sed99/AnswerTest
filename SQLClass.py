da7f0a2724fc819140ddca34b0efda6e5ee35c7e
Denis Belavin <sedred51@yandex.ru>
2017-12-12 22:15:47 +0300

    Create new branch (new_sqlclass). To simplify the syntax, in a class SQLClass.

--- SQLClass.py
+++ SQLClass.py
@@ -6,51 +6,11 @@ class SQLClass:
         self.connection = sqlite3.connect(database)
         self.cursor = self.connection.cursor()
 
-    def get_all(self, table):
-        """Получаем все строки таблицы"""
-        return self.cursor.execute('SELECT * FROM '
-                                   + str(table)).fetchall()
-
-
-    def count_rows(self, table):
-        """Считаем количесвто строк в таблице"""
+    def all_qwestins(self):
         with self.connection:
-            result = self.cursor.execute('SELECT * FROM '
-                                        + str(table)).fetchall()
-            return len(result)
+            res = self.cursor.execute('SELECT * FROM answers').fetchall()
+            return len(res)
 
-    def select_singl(self, table, col, row):
-        """Получаем одну строку из таблицы"""
-        with self.connection:
-            return self.cursor.execute('SELECT * FROM '
-                                       +str(table)+' WHERE '
-                                       +str(col) + ' = '
-                                       +str(row)).fetchall()[0]
-
-    def add_row(self, table, col, row):
-        """Добавляем запись в таблицу"""
-        with self.connection:
-            self.cursor.execute('INSERT INTO '+str(table)+
-                                ' ('+str(col)+') VALUES ('+str(row)+')')
-            self.connection.commit()
-
-
-    def del_row(self,table, col, row, text):
-        """Удаляем запись из таблицы"""
-        with self.connection:
-            self.cursor.execute('DELETE FROM '+str(table)+
-                                ' WHERE '+str(col)+' = '+str(row))
-            self.connection.commit()
-
-
-    def change_row(self, table,col,row,text, num):
-        """Перезаписываем строку из таблицы"""
-        with self.connection:
-            self.cursor.execute('UPDATE '+str(table)+
-                                ' SET ' +str(col)+
-                                ' = '+str(text)+
-                                ' WHERE '+str(row)+ ' = ' +str(num))
-            self.connection.commit()
 
 
     def close(self):
--- bot.py
+++ bot.py
@@ -26,15 +26,12 @@ def new_member(message):
     msg = bot.send_message(message.chat.id, 'Начнем', parse_mode='HTML',reply_markup=keyboard)
     bot.register_next_step_handler(msg, new_qwestion)
 
+@bot.message_handler(content_types='text')
 
-def new_qwestion(message):
-    rownum = modules.random_num()
+def all_answers(message):
     db = SQLClass(config.db)
-
-    res = db.select_singl(table=config.t_quest,
-                          col=config.id_q,row=rownum)
-    res = modules.parser_string(res)
-    print(res)
+    res = db.all_qwestins()
+    bot.send_message(message.chat.id, res)
 
 
 
