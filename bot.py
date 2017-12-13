2b9924a2a8edf9bc89cfc70f32c26f2644f858ab
Denis Belavin <sedred51@yandex.ru>
2017-12-13 14:58:11 +0300

    Small fixes.

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+#-*- koding: utf-8 -*-
+
 import telebot
 import config
 import status
@@ -13,25 +15,6 @@ keyboard = types.ReplyKeyboardMarkup(resize_keyboard=True, one_time_keyboard = T
 keyboard.row('Загадать препарат')
 
 
-
-@bot.message_handler(commands=['start'])
-
-def new_member(message):
-    db = SQLClass(config.db)
-    flag = db.del_row(table=config.t_chat,col=config.col_u,row=message.chat.id)
-    if flag == None:
-        db.add_row(table=config.t_chat,col=config.col_u,row=message.chat.id)
-    db.close()
-    keyboard.one_time_keyboard = True
-    msg = bot.send_message(message.chat.id, 'Начнем', parse_mode='HTML',reply_markup=keyboard)
-    bot.register_next_step_handler(msg, new_qwestion)
-
-@bot.message_handler(content_types='text')
-
-def all_answers(message):
-    db = SQLClass(config.db)
-    res = db.all_qwestins()
-    bot.send_message(message.chat.id, res)
 
 
 
